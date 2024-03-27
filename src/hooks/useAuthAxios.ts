7dc7fed315647d27cdcdeff6d7a4e72e7d51b0d2
HBSPS <wiyeong79@naver.com>
2024-03-27 12:04:39 +0900

    refactor: useAuthAxios 리팩토링

--- src/hooks/useAuthAxios.ts
+++ src/hooks/useAuthAxios.ts
@@ -1,10 +1,17 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import { useNavigate } from 'react-router-dom';
 import useSetToken from './useSetToken';
 import useToken from './useToken';
 import { REFRESH_TOKEN } from '../constants/auth';
 import { RefreshResponse } from '../types/api';
 
+function isAxiosError(error: unknown): error is AxiosError {
+  return (error as AxiosError).response !== undefined;
+}
+
 export default function useAuthAxios() {
+  const navigate = useNavigate();
+
   const { accessToken } = useToken();
   const { setAccessToken } = useSetToken();
 
@@ -14,6 +21,34 @@ export default function useAuthAxios() {
       'Content-Type': 'application/json',
     },
   });
+
+  async function getNewRefreshToken(requestConfig: AxiosRequestConfig) {
+    const originalRequest = { ...requestConfig };
+
+    try {
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
+      const refreshResponse = await axios.post<RefreshResponse>(`${import.meta.env.VITE_API_DOMAIN}/auth/token/refresh/`, {
+        refresh: refreshToken,
+      });
+      const { access: newAccessToken } = refreshResponse.data;
+
+      setAccessToken(newAccessToken);
+
+      if (!originalRequest.headers) originalRequest.headers = {};
+      originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return authAxios(originalRequest);
+    } catch (refreshError) {
+      if (isAxiosError(refreshError) && refreshError.response?.status === 401) {
+        localStorage.removeItem(REFRESH_TOKEN);
+        navigate('/login');
+      }
+
+      return Promise.reject(refreshError);
+    }
+  }
 
   authAxios.interceptors.request.use(
     (config) => {
@@ -32,24 +67,7 @@ export default function useAuthAxios() {
 
       if (error.response.status === 401 && !originalRequest.retry) {
         originalRequest.retry = true;
-
-        try {
-          const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-
-          const response = await axios.post<RefreshResponse>(`${import.meta.env.VITE_API_DOMAIN}/auth/token/refresh/`, {
-            refresh: refreshToken,
-          });
-
-          const { access: newAccessToken } = response.data;
-
-          setAccessToken(newAccessToken);
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-          // eslint-disable-next-line @typescript-eslint/return-await
-          return authAxios(originalRequest);
-        } catch (refreshError) {
-          return Promise.reject(refreshError);
-        }
+        return getNewRefreshToken(originalRequest);
       }
 
       return Promise.reject(error);

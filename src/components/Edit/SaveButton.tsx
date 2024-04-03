da6abd600604ee6a56eaeadd997d4a2aa3250052
HBSPS <wiyeong79@naver.com>
2024-04-03 22:02:20 +0900

    refactor: 삼항 연산자에서 createElement로 리팩토링

--- src/components/Edit/SaveButton.tsx
+++ src/components/Edit/SaveButton.tsx
@@ -1,13 +1,16 @@
+import { createElement } from 'react';
+
 interface SaveButtonProps {
   disabled: boolean;
 }
 
 export default function SaveButton({ disabled }: SaveButtonProps) {
-  return disabled ? (
-    <span className="rounded-lg bg-base-200 px-4 py-2 duration-300 hover:bg-base-300">Loading</span>
-  ) : (
-    <button type="submit" className="rounded-lg bg-base-200 px-4 py-2 duration-300 hover:bg-base-300">
-      Save
-    </button>
+  return createElement(
+    disabled ? 'span' : 'button',
+    {
+      className: 'rounded-lg bg-base-200 px-4 py-2 duration-300 hover:bg-base-300',
+      type: disabled ? undefined : 'submit',
+    },
+    disabled ? 'Loading' : 'Save',
   );
 }
--- src/components/Login/LoginButton.tsx
+++ src/components/Login/LoginButton.tsx
@@ -1,18 +1,16 @@
+import { createElement } from 'react';
+
 interface LoginButtonProps {
   isLoading: boolean;
 }
 
 export default function LoginButton({ isLoading }: LoginButtonProps) {
-  return isLoading ? (
-    <span className="col-span-1 flex h-full w-full items-center justify-center rounded-md bg-base-200 mobile:ml-0 mobile:mt-2 mobile:h-10">
-      Loading...
-    </span>
-  ) : (
-    <button
-      className="col-span-1 flex h-full w-full items-center justify-center rounded-md bg-base-200 mobile:ml-0 mobile:mt-2 mobile:h-10"
-      type="submit"
-    >
-      로그인
-    </button>
+  return createElement(
+    isLoading ? 'span' : 'button',
+    {
+      className: 'col-span-1 flex h-full w-full items-center justify-center rounded-md bg-base-200 mobile:ml-0 mobile:mt-2 mobile:h-10',
+      type: isLoading ? undefined : 'submit',
+    },
+    isLoading ? 'Loading...' : '로그인',
   );
 }

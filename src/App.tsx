91a6f08844fb508cd0e02c5fff94b65b47be98cb
HBSPS <wiyeong79@naver.com>
2024-04-08 23:57:52 +0900

    fix: App.tsx 컴포넌트 절대 경로로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LayoutWithNav from './pages/layouts/LayoutWithNav';
-import Home from './pages/Home';
-import Wiki from './pages/Wiki';
-import LayoutWithoutNav from './pages/layouts/LayoutWithoutNav';
-import Login from './pages/Login';
-import Edit from './pages/Edit';
-import SignUp from './pages/SignUp';
+import LayoutWithNav from '@/pages/layouts/LayoutWithNav';
+import Home from '@/pages/Home';
+import Wiki from '@/pages/Wiki';
+import LayoutWithoutNav from '@/pages/layouts/LayoutWithoutNav';
+import Login from '@/pages/Login';
+import Edit from '@/pages/Edit';
+import SignUp from '@/pages/SignUp';
 
 export default function App() {
   const router = createBrowserRouter([

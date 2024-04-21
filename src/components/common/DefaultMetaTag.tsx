f74981f8a97c8d162374602fff97188d9b68a553
HBSPS <wiyeong79@naver.com>
2024-04-21 14:02:05 +0900

    feat: react-helmet-async 라이브러리 추가

---
+++ src/components/common/DefaultMetaTag.tsx
@@ -0,0 +1,9 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function DefaultMetaTag() {
+  return (
+    <Helmet>
+      <title>산보실록</title>
+    </Helmet>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import '@/styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+);

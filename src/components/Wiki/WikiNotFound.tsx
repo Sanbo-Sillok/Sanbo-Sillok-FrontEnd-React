786745f20c0d0d739f510d85decbbceb4dc81740
HBSPS <wiyeong79@naver.com>
2024-04-22 17:26:24 +0900

    feat: Wiki 페이지 title 추가

--- src/components/Wiki/WikiNotFound.tsx
+++ src/components/Wiki/WikiNotFound.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import WikiPageTitle from './WikiPageTitle';
 
 interface WikiNotFoundProps {
@@ -7,13 +8,18 @@ interface WikiNotFoundProps {
 
 export default function WikiNotFound({ pageTitle }: WikiNotFoundProps) {
   return (
-    <div className="min-h-screen p-10 dark:bg-base-800">
-      <WikiPageTitle>{pageTitle}</WikiPageTitle>
-      <p className="mt-6 dark:text-base-200">페이지가 존재하지 않습니다.</p>
-      <br />
-      <Link to={`/edit/${pageTitle}`} className="text-sanbo-blue">
-        [페이지 생성하기]
-      </Link>
-    </div>
+    <>
+      <Helmet>
+        <title>산보실록: {pageTitle}</title>
+      </Helmet>
+      <div className="p-10">
+        <WikiPageTitle>{pageTitle}</WikiPageTitle>
+        <p className="mt-6 dark:text-base-200">페이지가 존재하지 않습니다.</p>
+        <br />
+        <Link to={`/edit/${pageTitle}`} className="text-sanbo-blue">
+          [페이지 생성하기]
+        </Link>
+      </div>
+    </>
   );
 }

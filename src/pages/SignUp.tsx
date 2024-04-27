5f0d7d541b4103353e1639a86a62e616df3b2709
HBSPS <wiyeong79@naver.com>
2024-04-27 14:12:22 +0900

    feat: SignUp 페이지 title 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,16 @@
+import { Helmet } from 'react-helmet-async';
 import SignupForm from '@/components/Signup/SignupForm';
 
 export default function SignUp() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-5 p-10">
-      <img src="/favicon.ico" alt="산보실록 아이콘" width={100} height={100} />
-      <SignupForm />
-    </div>
+    <>
+      <Helmet>
+        <title>산보실록: 회원가입</title>
+      </Helmet>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-5 p-10">
+        <img src="/favicon.ico" alt="산보실록 아이콘" width={100} height={100} />
+        <SignupForm />
+      </div>
+    </>
   );
 }

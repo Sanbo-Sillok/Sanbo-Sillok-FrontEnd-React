335abc44f376a1970bed9549943c6b7b02a469cd
HBSPS <wiyeong79@naver.com>
2024-05-06 19:32:14 +0900

    feat: PWA manifest 추가
    TODO: manifest 수정 및 apple icon 추가

--- src/components/common/DefaultMetaTag.tsx
+++ src/components/common/DefaultMetaTag.tsx
@@ -1,17 +1,26 @@
 import { Helmet } from 'react-helmet-async';
+import IOSMetaTag from './IOSMetaTag';
 
 export default function DefaultMetaTag() {
   return (
-    <Helmet>
-      <title>산보실록</title>
+    <>
+      <Helmet>
+        <title>산보실록</title>
 
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="산보실록" />
-      <meta property="og:site_name" content="산보실록" />
-      <meta property="og:description" content="산보실록 - 함께 만들어가는 페이지" />
-      {/* FIXME: 배포 경로로 수정 */}
-      <meta property="og:image" content="" />
-      <meta property="og:url" content="" />
-    </Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="산보실록" />
+        <meta property="og:site_name" content="산보실록" />
+        <meta property="og:description" content="산보실록 - 함께 만들어가는 페이지" />
+        {/* FIXME: 배포 경로로 수정 */}
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="" />
+
+        <link rel="manifest" href="manifest.json" />
+
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#F4F4F5" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#27272A" />
+      </Helmet>
+      <IOSMetaTag />
+    </>
   );
 }

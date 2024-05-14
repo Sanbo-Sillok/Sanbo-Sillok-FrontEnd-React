b06364cdddde3393da371c20f4742bc4ae95827d
HBSPS <wiyeong79@naver.com>
2024-05-14 16:18:01 +0900

    feat: 툴팁 CSS 추가

--- src/components/NavBar/RandomTitleButton.tsx
+++ src/components/NavBar/RandomTitleButton.tsx
@@ -10,6 +10,8 @@ export default function RandomTitleButton() {
       aria-label="random wiki"
       disabled={isLoading}
       onClick={navigateRandomPage}
+      data-tooltip="랜덤 페이지 이동"
+      data-flow="down"
       className="flex h-10 w-10 items-center justify-center rounded-lg duration-200 hover:bg-base-600 hover:bg-opacity-30"
     >
       <RandomSVG />

2e447dfd2f51bd03799e50d0baf376bbb6ce446a
HBSPS <wiyeong79@naver.com>
2024-05-25 10:08:58 +0900

    feat: 로그아웃시 사용자 권한 정보 제거

--- src/apis/mutations/useLogoutMutation.ts
+++ src/apis/mutations/useLogoutMutation.ts
@@ -5,10 +5,12 @@ import useAuthAxiosInstance from '@/hooks/useAuthAxiosInstance';
 import useSetToken from '@/hooks/auth/useSetToken';
 import { LogoutResponse } from '@/types/apis/auth';
 import useLocalStorage from '@/hooks/useLocalStorage';
+import useUserRole from '@/hooks/auth/useUserRole';
 
 export default function useLogoutMutation() {
   const authAxios = useAuthAxiosInstance();
   const { setAccessToken, setRefreshToken } = useSetToken();
+  const { setUserRole } = useUserRole();
   const [, , removeAccessToken] = useLocalStorage(ACCESS_TOKEN, null);
   const navigate = useNavigate();
 
@@ -22,6 +24,7 @@ export default function useLogoutMutation() {
     localStorage.removeItem(REFRESH_TOKEN);
     setAccessToken(null);
     setRefreshToken(null);
+    setUserRole(null);
 
     // FIXME: refresh token으로 변경
     removeAccessToken();

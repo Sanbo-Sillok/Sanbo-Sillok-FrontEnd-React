5787ec781afe967702b934d659e5aee9c87fb635
HBSPS <wiyeong79@naver.com>
2024-06-29 22:24:27 +0900

    feat: 모든 페이지 조회 페이지 추가

--- src/apis/queries/useAllTitleQuery.ts
+++ src/apis/queries/useAllTitleQuery.ts
@@ -1,18 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { REACT_QUERY_KEYS } from '@/constants/queryKey';
 import useAuthAxiosInstance from '@/hooks/useAuthAxiosInstance';
+import { WikiTitle } from '@/types/wiki';
 
 export function useAllTitleQuery() {
   const authAxios = useAuthAxiosInstance();
 
   const getAllTitles = async () => {
-    const response = await authAxios.get('/post');
+    const response = await authAxios.get<WikiTitle[]>('/post');
 
-    return response.data;
+    return response.data.sort((a, b) => (a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase() ? -1 : 1));
   };
 
   return useQuery({
     queryKey: [REACT_QUERY_KEYS.ALL_TITLES],
     queryFn: getAllTitles,
+    staleTime: 10 * 1000,
   });
 }

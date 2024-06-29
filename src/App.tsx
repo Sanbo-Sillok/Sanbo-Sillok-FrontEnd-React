3a5dc5cd2caaa27ca3ababbc00abddbb927f0f18
HBSPS <wiyeong79@naver.com>
2024-06-30 00:23:56 +0900

    fix: vercel analytics import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import LayoutErrorBoundary from '@/pages/layouts/LayoutErrorBoundary';
 import LayoutWithNav from '@/pages/layouts/LayoutWithNav';
 import LayoutWithoutNav from '@/pages/layouts/LayoutWithoutNav';
